04ee1ce9149c33306fc516f1c58ff908432d86f3
Adam Weis <aweis@andrew.cmu.edu>
2012-04-29 23:04:02 -0400

    python stuff

---
+++ apache-tomcat/webapps/timestamps/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+  <title>Hello.jsp</title>
+</head>
+
+  <h1><%= new String("Hello World") %></h1> 
+
+  <p>
+    <%= 9+9.0 %>
+  </p>
+
+</html>
---
+++ apache-tomcat/work/Catalina/localhost/java/org/apache/jsp/hello_jsp.java
@@ -0,0 +1,88 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class hello_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<title>Sample Application JSP Page</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("<body bgcolor=white>\r\n");
+      out.write("\r\n");
+      out.write("<table border=\"0\">\r\n");
+      out.write("<tr>\r\n");
+      out.write("<td align=center>\r\n");
+      out.write("<img src=\"images/tomcat.gif\">\r\n");
+      out.write("</td>\r\n");
+      out.write("<td>\r\n");
+      out.write("<h1>Sample Application JSP Page</h1>\r\n");
+      out.write("This is the output of a JSP page that is part of the Hello, World\r\n");
+      out.write("application.\r\n");
+      out.write("</td>\r\n");
+      out.write("</tr>\r\n");
+      out.write("</table>\r\n");
+      out.write("\r\n");
+      out.print( new String("Hello!") );
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
